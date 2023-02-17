e5eb5b611f5c750e60b9f7bdee301ce8b95ca326
Rishi Vasa <rs310602@dal.ca>
2023-02-17 18:59:36 -0400

    Update index.js

--- tutorial3/src/index.js
+++ tutorial3/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import ProfilePage from './ProfileComponents/ProfilePage';
 import Registration from './RegistrationComponents/Register';
 import reportWebVitals from './reportWebVitals';
