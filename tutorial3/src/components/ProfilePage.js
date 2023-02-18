2b9a984acd610402b072f52f42f22e9a24fa541d
Rishi Vasa <rs310602@dal.ca>
2023-02-17 21:46:59 -0400

    Implemented form validations and profile page hardcoding after successful form submission

--- tutorial3/src/components/ProfilePage.js
+++ tutorial3/src/components/ProfilePage.js
@@ -1,19 +1,33 @@
+import * as React from 'react';
+import * as Mui from '@mui/material';
 import Link from '@mui/material/Link';
 
+//Reference for dark Mode: https://mui.com/material-ui/customization/dark-mode/
+const theme = Mui.createTheme({ palette: { mode: 'dark' } });
 
-function Profile() {
+export default function Profile() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          This is the website Profile page!<br/>
-          <Link href = "/">
-            Semi Functional Prototype INCOMING for USER DASHBOARD (After User Logs in)!!!!!!!
-          </Link>
-        </p>
-      </header>
-    </div>
+    <Mui.ThemeProvider theme={theme}>
+      <Mui.Container component="main" maxWidth="xs">
+        <Mui.CssBaseline />
+        <Mui.Box sx={{ mt: 5 }}>
+
+          <Mui.Typography variant="h6">This is the hardcoded initial Profile page after successful login!</Mui.Typography>
+
+          <Mui.Box sx={{ mt: 5 }}>
+            <Mui.Typography variant="h6"> First Name: Rishi</Mui.Typography>
+            <Mui.Typography variant="h6"> Last Name: Vasa</Mui.Typography>
+            <Mui.Typography variant="h6"> Email: B00902815@dal.ca</Mui.Typography>
+          </Mui.Box>
+
+          <Mui.Box sx={{ mt: 5 }}>
+            <Link href="/">
+              Go Back to Registration Page!
+            </Link>
+          </Mui.Box>
+
+        </Mui.Box>
+      </Mui.Container>
+    </Mui.ThemeProvider>
   );
 }
-
-export default Profile;
--- tutorial3/src/components/Register.js
+++ tutorial3/src/components/Register.js
@@ -1,99 +1,91 @@
 import * as React from 'react';
 import * as Mui from '@mui/material';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { useNavigate } from "react-router-dom";
+import validator from 'validator'
 
-const theme = Mui.createTheme();
+//Reference for Form Validation: https://www.npmjs.com/package/validator
+//Reference for Form Page Template: https://mui.com/material-ui/getting-started/templates/
+
+//Reference for dark Mode: https://mui.com/material-ui/customization/dark-mode/
+const theme = Mui.createTheme({palette: {mode: 'dark'}});
 
 export default function Register() {
     const navigate = useNavigate();
     const handleSubmit = (event) => {
-        navigate("/profile");
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const firstName = data.get('firstName');
+        const lastName = data.get('lastName');
+        const email = data.get('email');
+        const password = data.get('password');
+        const confirmPassword = data.get('confirmPassword');
+
+        if (!validator.isAlpha(firstName)) {
+            alert('First Name should contain only alphabets!');
+            return;
+        }
+        if (!validator.isAlpha(lastName)) {
+            alert('Last Name should contain only alphabets!');
+            return;
+        }
+        if (!validator.isEmail(email)) {
+            alert('Enter Valid Email ID!');
+            return;
+        }
+        else if (!validator.isLength(password, {min:8})) {
+            alert('Password length should be greater than 8 characters');
+            return;
+        }
+        else if(!validator.isStrongPassword(password, {minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1})){
+            alert('Password should contain at least 1 lowercase, uppercase, numeric and special character');
+            return;
+        }
+        else if (!validator.equals(password, confirmPassword)) {
+            alert('Passwords Do Not Match!');
+            return;
+        }
+        else {
+            navigate("/profile");
+        }
     };
 
     return (
         <Mui.ThemeProvider theme={theme}>
             <Mui.Container component="main" maxWidth="xs">
                 <Mui.CssBaseline />
-                <Mui.Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Mui.Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                        <LockOutlinedIcon />
-                    </Mui.Avatar>
-                    <Mui.Typography component="h1" variant="h5">
-                        Sign up
-                    </Mui.Typography>
-                    <Mui.Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                <Mui.Box sx={{ mt: 5 }}>
+
+                    <Mui.Typography variant="h6">Tutorial 3 Registration Form</Mui.Typography>
+
+                    <Mui.Box component="form" onSubmit={handleSubmit} sx={{ mt: 5 }}>
                         <Mui.Grid container spacing={2}>
-                            <Mui.Grid item xs={12} sm={6}>
-                                <Mui.TextField
-                                    autoComplete="given-name"
-                                    name="firstName"
-                                    required
-                                    fullWidth
-                                    id="firstName"
-                                    label="First Name"
-                                    autoFocus
-                                />
+                            
+                            <Mui.Grid item>
+                                <Mui.TextField id="firstName" name="firstName" label="First Name" autoFocus required />
                             </Mui.Grid>
-                            <Mui.Grid item xs={12} sm={6}>
-                                <Mui.TextField
-                                    required
-                                    fullWidth
-                                    id="lastName"
-                                    label="Last Name"
-                                    name="lastName"
-                                    autoComplete="family-name"
-                                />
+                            
+                            <Mui.Grid item>
+                                <Mui.TextField id="lastName" name="lastName" label="Last Name" required />
                             </Mui.Grid>
-                            <Mui.Grid item xs={12}>
-                                <Mui.TextField
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    label="Email Address"
-                                    name="email"
-                                    autoComplete="email"
-                                />
+
+                            <Mui.Grid item>
+                                <Mui.TextField id="email" name="email" label="Email Address" type="email" required />
                             </Mui.Grid>
-                            <Mui.Grid item xs={12}>
-                                <Mui.TextField
-                                    required
-                                    fullWidth
-                                    name="password"
-                                    label="Password"
-                                    type="password"
-                                    id="password"
-                                    autoComplete="new-password"
-                                />
+                            
+                            <Mui.Grid item>
+                                <Mui.TextField id="password" name="password" label="Password" type="password" required />
                             </Mui.Grid>
+
+                            <Mui.Grid item>
+                                <Mui.TextField id="confirmPassword" name="confirmPassword" label="Confirm Password" type="password" required />
+                            </Mui.Grid>
+
                         </Mui.Grid>
-                        <Mui.Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            Sign Up
-                        </Mui.Button>
+                        
+                        <Mui.Button type="submit" variant="contained" sx={{ mt: 3 }}>Sign Up</Mui.Button>
+                    
                     </Mui.Box>
                 </Mui.Box>
-                <div className="App">
-                    <header className="App-header">
-                        <p>
-                            This is the website Registration page!<br />
-                            <Mui.Link href="/profile">
-                                Semi Functional Prototype INCOMING for USER DASHBOARD (After User Logs in)!!!!!!!
-                            </Mui.Link>
-                        </p>
-                    </header>
-                </div>
             </Mui.Container>
         </Mui.ThemeProvider>
     );
