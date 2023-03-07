0c5bad82e998483c998a5d87e5f38f4739fad988
Rishi Vasa <rs310602@dal.ca>
2023-03-07 02:19:12 -0400

    Update UserProfile.jsx

--- tutorial4/src/components/Profiles/UserProfile.jsx
+++ tutorial4/src/components/Profiles/UserProfile.jsx
@@ -1,20 +1,38 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as Mui from '@mui/material';
+import { useLocation } from 'react-router-dom'
 
 //Reference for dark Mode: https://mui.com/material-ui/customization/dark-mode/
 const theme = Mui.createTheme({ palette: { mode: 'dark' } });
 
-export default function UserProfile(props) {
-  return (
-    <Mui.ThemeProvider theme={theme}>
-      <Mui.Container component="main" maxWidth="md">
-        <Mui.CssBaseline />
-        <Mui.Box sx={{ mt: 5 }}>
+export default function UserProfile() {
 
-          <Mui.Typography variant="h6">This is the hardcoded initial Profile page after successful login!</Mui.Typography>
+    const { state } = useLocation();
 
-        </Mui.Box>
-      </Mui.Container>
-    </Mui.ThemeProvider>
-  );
+    const [userDetails, setUserDetails] = useState([]);
+
+
+    useEffect(() => {
+        fetch('https://express-t4.onrender.com/api/users/' + state.user._id)
+            .then(results => results.json())
+            .then(data => { console.log(data); setUserDetails(data) })
+    }, []);
+
+    return (
+        <Mui.ThemeProvider theme={theme}>
+            <Mui.Container component="main" maxWidth="md">
+                <Mui.CssBaseline />
+                <Mui.Box sx={{ mt: 5 }}>
+
+                    <Mui.Typography variant="h6">User Profile for {userDetails.name}</Mui.Typography>
+
+                    <Mui.Box sx={{ mt: 5 }}>
+                    <img src ={userDetails.picture} alt="testpic" width="200" height="200" />
+                    <pre>{JSON.stringify(userDetails, null, 2)}</pre>
+
+                    </Mui.Box>
+                </Mui.Box>
+            </Mui.Container>
+        </Mui.ThemeProvider>
+    );
 }
